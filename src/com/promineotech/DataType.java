cafd03c9dda0da376604e72207f9f97867bc09db
Kmac01 <117133875+Kmac01@users.noreply.github.com>
2022-12-03 18:46:38 -0500

    haha i am a phishing link

---
+++ src/com/promineotech/DataType.java
@@ -0,0 +1,17 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class DoubleExample1 {  
+			  
+		    public static void main(String[] args) {  
+		          
+				Double a = 2.748;
+				
+				checkType(a);
+		    }
+}
+	}
+}
